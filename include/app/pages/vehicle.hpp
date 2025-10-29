469d938adc66df9ed504709d71607a3f487edb7e
SamiFy <sami.syr.fy@gmail.com>
2025-10-29 18:19:08 +0100

    Added Climate Controls buttons - POC

--- include/app/pages/vehicle.hpp
+++ include/app/pages/vehicle.hpp
@@ -7,6 +7,7 @@
 #include <app/widgets/XMFramedWidget.hpp>
 #include <qobjectdefs.h>
 #include <qwidget.h>
+#include <QProgressBar>
 
 #include "canbus/socketcanbus.hpp"
 #include "obd/message.hpp"
@@ -95,21 +96,53 @@ private:
     QLabel *statusLabel;
 };
 
-class ClimateControlsWidget : public FramedWidget {
+class ClimateControlsWidget : public FramedWidget
+{
     Q_OBJECT
 
-    public:
-        ClimateControlsWidget(QWidget *parent = nullptr);
+public:
+    // Change "QWidget" back to "FramedWidget" here too.
+    explicit ClimateControlsWidget(QWidget *parent = nullptr);
 
-    public:
-        //PlaceHolders
-        void readTemperatures(float outsideTemp, float cabinTemp, float engineTemp);
-        void writeControls();
+private slots:
+    // Slots to handle button clicks
+    void onBlowerPlus();
+    void onBlowerMinus();
+    void onTempPlus();
+    void onTempMinus();
 
-    private:
-        QLabel *FanRPM;
-        QLabel *setTemp;
+private:
+    // Helper functions to update UI
+    void updateTemperatureLabel();
+    void updateBlowerBar();
+
+    // Blower Controls
+    QPushButton* m_blowerMinusButton;
+    QPushButton* m_blowerPlusButton;
+    QProgressBar* m_blowerSpeedBar;
+
+    // Temperature Controls
+    QPushButton* m_tempMinusButton;
+    QPushButton* m_tempPlusButton;
+    QLabel* m_temperatureLabel;
+
+    // Function Buttons
+    QPushButton* m_circulateButton;
+    QPushButton* m_flowButton;
+    QPushButton* m_defogButton;
+    QPushButton* m_rearHeatButton;
+
+    // State variables
+    int m_blowerSpeed;
+    int m_temperature;
+
+    // Constants
+    static const int TEMP_MIN = 16;
+    static const int TEMP_MAX = 30;
+    static const int BLOWER_MAX = 10;
 };
+
+
 
 class ClockWidget : public FramedWidget {
     Q_OBJECT
